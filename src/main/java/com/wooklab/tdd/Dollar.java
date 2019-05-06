048e55d50cc683e6b6d8f0429784480f181242be
wookmac <wookmac@wookmac.local>
2019-05-06 16:43:48 +0900

    다중 통화를 지원하는 Money객체

---
+++ src/main/java/com/wooklab/tdd/Dollar.java
@@ -0,0 +1,12 @@
+package com.wooklab.tdd;
+
+public class Dollar {
+    int amount;
+    public Dollar(int amount) {
+        this.amount = amount;
+    }
+
+    void times(int multiplier) {
+        amount *= multiplier;
+    }
+}
---
+++ src/test/java/com/wooklab/tdd/TestMain.java
@@ -0,0 +1,18 @@
+package com.wooklab.tdd;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * 통화가 다른 두 금액을 더해서 주어진 환율에 맞게 변한 금액을 결과로 얻을 수 있어야 한다.
+ * 어떤 금액(주가)을 어떤 수(주식의 수)에 곱한 금액을 결과로 얻을 수 있어야 한다.
+ */
+public class TestMain {
+    @Test
+    public void testMultiplication() {
+        Dollar five = new Dollar(5);
+        five.times(2);
+        assertEquals(10, five.amount);
+    }
+}
